4ff3ce46882877d8f7d21a6413506d5c0c3e6cb4
mecharn2 <mecharn2@gmail.com>
2024-11-18 16:44:12 +0100

    responci1

---
+++ js/presupuestoOriginal.js
@@ -0,0 +1,89 @@
+const nombreInput = document.getElementById('nombre');
+const apellidosInput = document.getElementById('apellidos');
+const telefonoInput = document.getElementById('telefono');
+const emailInput = document.getElementById('email');
+const formulario = document.getElementById('form');
+
+function validarNombre(){
+    const nombre = nombreInput.value
+    const nombrePattern = /^[a-zA-ZÑñÁáÉéÍíÓóÚú ]{4,15}$/;
+    if (nombre.length >= 4 && nombre.length <= 15 && nombrePattern.test(nombre)) {
+        nombreInput.classList.add('valido');
+        nombreInput.classList.remove('invalido');
+        document.getElementById('nombreError').textContent = ''
+    }else {
+        nombreInput.classList.add('invalido');
+        nombreInput.classList.remove('valido');
+        document.getElementById('nombreError').textContent = 'El nombre debe de tener un mínimo de 4 y un máximo de 15 letras.'
+    }
+};
+function validarApellidos(){
+    const apellidos = apellidosInput.value
+    const apellidosPattern = /^[a-zA-ZÑñÁáÉéÍíÓóÚú ]{10,40}$/;
+    if (apellidos.length >=10 && apellidos.length <= 40 && apellidosPattern.test(apellidos)) {
+    apellidosInput.classList.add('valido');
+    apellidosInput.classList.remove('invalido');
+    document.getElementById('apellidosError').textContent = ''
+    }else{
+    apellidosInput.classList.add('invalido')
+    apellidosInput.classList.remove('valido')
+    document.getElementById('apellidosError').textContent = 'Los apellidos deben de tener un mínimo de 10 y un máximo de 40 letras.'
+    }
+};
+function validarTelefono(){
+    const telefono = telefonoInput.value
+    const telefonoPattern = /^\d{9}$/
+    if (telefonoPattern.test(telefono)) {
+        telefonoInput.classList.add('valido');
+        telefonoInput.classList.remove('invalido');
+        document.getElementById('telefonoError').textContent = ''
+    }else{
+        telefonoInput.classList.add('invalido');
+        telefonoInput.classList.remove('valido');
+        document.getElementById('telefonoError').textContent = 'El número de telefono debe tener 9 dígitos y contener solo números.'
+        }
+};
+function validarEmail(){
+    const email = emailInput.value
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (emailPattern.test(email)) {
+        emailInput.classList.add('valido');
+        emailInput.classList.remove('invalido');
+        document.getElementById('emailError').textContent = ''
+    }else{
+        emailInput.classList.add('invalido');
+        emailInput.classList.remove('valido');
+        document.getElementById('emailError').textContent = 'Ingrese un correo electrónico correcto.'
+        }
+};
+
+function resetFormulario() {
+    formulario.reset()
+    nombreInput.classList.remove('valido');
+    apellidosInput.classList.remove('valido');
+    telefonoInput.classList.remove('valido');
+    emailInput.classList.remove('valido');
+    
+};
+nombreInput.addEventListener('input', validarNombre);
+apellidosInput.addEventListener('input', validarApellidos);
+telefonoInput.addEventListener('input', validarTelefono);
+emailInput.addEventListener('input', validarEmail);
+
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault()
+    validarNombre()
+    validarApellidos()
+    validarTelefono()
+    validarEmail()
+    
+
+    if(nombreInput.classList.contains('valido') && apellidosInput.classList.contains('valido') && telefonoInput.classList.contains('valido') && emailInput.classList.contains('valido')) {
+        alert('Formulario enviado correctamente');
+        resetFormulario();
+    } else {
+        alert('Por favor, corrija los errores en el formulario')
+    }
+})
+
